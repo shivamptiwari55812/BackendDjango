411123d5e5f1ee31f401c10222c2725168203dd2
Shivam <Shivamptiwari55812@gmail.com>
2025-04-18 23:53:09 +0530

    Pushing code

--- warehouseminiBack/settings.py
+++ warehouseminiBack/settings.py
@@ -96,14 +96,22 @@ WSGI_APPLICATION = 'warehouseminiBack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('INTERNAL_BASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
-
+if 'RENDER' not in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    # Use Render's PostgreSQL database in production
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('INTERNAL_BASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
